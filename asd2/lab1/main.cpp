cd6f3ccd7a9c109344ecee9474f71399c49d7f49
Alexandr <ARTrofimenko@stud.kantiana.ru>
2023-03-24 23:51:27 +0200

    add lab

---
+++ asd2/lab1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+void CreateRuns(int S,vector<int> mass)
+{
+	ifstream in("input.txt");
+	ofstream on1("A.txt");
+	
+
+	while (!in.eof()) {
+
+	}
+}
+
+
+int main()
+{
+
+	return 0;
+}
